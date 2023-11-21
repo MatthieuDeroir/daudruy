8bcc3c94cde872662c95f3911cdb4944a4b43616
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-11-21 15:10:33 +0100

    Case

--- Backend/Routes/VeilleRoutes.js
+++ Backend/Routes/VeilleRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const veilleController = require("../Controllers/VeilleController");
 
-const authMiddleware = require("../Middlewares/authMiddleware");
+const authMiddleware = require("../Middlewares/AuthMiddleware");
 
 router
   .route("/get-veille")
