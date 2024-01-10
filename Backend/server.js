5dfc824d3a591b6ce66a8387851e7e6db69dbb4c
MatthieuDeroir <matthieu.deroir@epsi.fr>
2024-01-10 17:29:41 +0100

    fix: remove MongoDb connection obsolete

--- Backend/server.js
+++ Backend/server.js
@@ -12,10 +12,6 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-// Connexion à MongoDB
-mongoose.connect('mongodb://localhost:27017/truckDisplay', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Import des modèles
 const Camion = require('./Models/CamionModel');
