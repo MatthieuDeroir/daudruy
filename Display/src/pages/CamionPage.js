c5a3425f9c9bf46ee600be8f5ac6447b83afec60
MatthieuDeroir <matthieu.deroir@epsi.fr>
2024-01-18 17:33:39 +0100

    add: image instead of go span

--- Display/src/pages/CamionPage.js
+++ Display/src/pages/CamionPage.js
@@ -1,84 +1,71 @@
 import React from "react";
 
-function CamionPage({ camionsData }) {
-  const getFontSize = (text, sizeLarge, sizeMedium, sizeSmall, limitLarge, limitSmall) => {
-    if (text.length > limitLarge) {
-      return sizeSmall;
-    } else if (text.length > limitSmall) {
-      return sizeMedium;
-    } else {
-      return sizeLarge;
-    }
-  };
-
-  const getCellStyle = (text, fontSize) =>
-{
-    let style = {
-      textAlign: "center",
-      width: "55%",
-      fontSize: fontSize,
-      margin: "-30px",
-      padding: "0px"
+function CamionPage({camionsData}) {
+    const getFontSize = (text, sizeLarge, sizeMedium, sizeSmall, limitLarge, limitSmall) => {
+        if (text.length > limitLarge) {
+            return sizeSmall;
+        } else if (text.length > limitSmall) {
+            return sizeMedium;
+        } else {
+            return sizeLarge;
+        }
     };
 
-    // Assumption: Each character is around 10px wide when font size is "30px"
-    // This is a rough estimation and may not be accurate for all fonts and characters
-    const estimatedTextWidth = text.length * 10;
+    const getCellStyle = (text, fontSize) => {
+        let style = {
+            textAlign: "center",
+            fontSize: fontSize,
+            margin: "0px",
+            padding: "0px",
+            overflow: "hidden",
+        };
 
-    if (fontSize === "30px" && estimatedTextWidth > 462) {
-      style = {...style, whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"};
-    }
+        // Assumption: Each character is around 10px wide when font size is "30px"
+        // This is a rough estimation and may not be accurate for all fonts and characters
+        const estimatedTextWidth = text.length * 10;
 
-    return style;
-  };
+        if (fontSize === "30px" && estimatedTextWidth > 462) {
+            style = {...style, whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"};
+        }
 
-    const DestinationIcon = ({ destination }) => {
+        return style;
+    };
+
+    const DestinationIcon = ({destination}) => {
         if (destination === 'Accueil') {
-            return <img src={`./A.png`} alt="Acceuil" style={{width: "60px", height: "60px"}} />;
+            return <img src={`./A.png`} alt="Acceuil" style={{width: "60px", height: "60px"}}/>;
         } else if (destination === 'Balance') {
-            return <img src={`./B.png`} alt="Balance" style={{width: "60px", height: "60px"}} />;
+            return <img src={`./B.png`} alt="Balance" style={{width: "60px", height: "60px"}}/>;
         } else {
             return null;
         }
     };
 
-  return (
-    <table style={{ width: "432", height: "216", borderSpacing: "0px" }}>
-      {camionsData.map((camion, index) =>
-        camion.action === "go" ? (
-          <tr key={index} style={{ height: "60px" }}>
-            <td style={getCellStyle(camion.immatriculation, getFontSize(camion.immatriculation, "50px", "40px", "30px", 9, 7))}>
-              {camion.immatriculation}
-            </td>
-            <td style={{ width: "20%", padding: "0px" }}>
-                <img
-                    src="go.png" // Remplacez par le chemin réel de votre image
-                    alt="Go"
-                    style={{ height:"60px" }}
-                />
-              {/*<div*/}
-              {/*  style={{*/}
-              {/*    width: "0px",*/}
-              {/*    height: "0px",*/}
-              {/*    borderLeft: "25px solid transparent",*/}
-              {/*    borderRight: "25px solid transparent",*/}
-              {/*    borderBottom: "50px solid #00a13b",*/}
-              {/*    position: "relative",*/}
-              {/*    transform: "rotate(90deg)",*/}
-              {/*  }}*/}
-              {/*>*/}
-
-
-              {/*</div>*/}
-            </td>
-            <td>
-                <DestinationIcon destination={camion.destination} />
-            </td>
-          </tr>
-        ) : <tr key={index} style={{ height: "60px" }}></tr>
-      )}
-    </table>
-  );
+    return (
+        <table style={{width: "432px", height: "216px", borderSpacing: "0px"}}>
+            {camionsData.map((camion, index) => (
+                camion.action === "go" ? (
+                    <tr key={index} style={{height: "33%"}}> {/* Equal height for each row */}
+                        <td style={{width: "40%", ...getCellStyle(camion.immatriculation, getFontSize(camion.immatriculation, "50px", "40px", "30px", 9, 7))}}>
+                            {camion.immatriculation}
+                        </td>
+                        <td style={{width: "20%"}}>
+                            <img
+                                src="go.png" // Remplacez par le chemin réel de votre image
+                                alt="Go"
+                                style={{height: "60px"}}
+                            />
+                        </td>
+                        <td style={{width: "40%"}}>
+                            <DestinationIcon destination={camion.destination}/>
+                        </td>
+                    </tr>
+                ) : (
+                    <tr key={index} style={{height: "33%"}}></tr> // Ensure all rows have equal height
+                )
+            ))}
+        </table>
+    );
 }
 
 export default CamionPage;
